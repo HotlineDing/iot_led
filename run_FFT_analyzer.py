2b5141a51b5a366481c5b2b59512b4208d86b8ab
Alexander Ding <ading1770@gmail.com>
2020-07-26 23:44:15 -0500

    Tweaked color and demo profiles

--- run_FFT_analyzer.py
+++ run_FFT_analyzer.py
@@ -6,7 +6,7 @@ ear = Stream_Analyzer(
                 device = None,               # Manually play with this (int) if you don't see anything
                 rate   = None,               # Audio samplerate, None uses the default source settings
                 FFT_window_size_ms  = 60,    # Window size used for the FFT transform
-                updates_per_second  = 1200,  # How often to read the audio stream for new data
+                updates_per_second  = 1000,  # How often to read the audio stream for new data
                 smoothing_length_ms = 50,    # Apply some temporal smoothing to reduce noisy features
                 n_frequency_bins    = 50,   # The FFT features are grouped in bins
                 visualize = 1,               # Visualize the FFT features with PyGame
@@ -14,8 +14,10 @@ ear = Stream_Analyzer(
                 )
 
 Leds = led.Led(ear.visualizer)
-Array = led.Array(ear.visualizer, 'Array', '192.168.0.150', 7777, 10, 15)
+Array = led.Array(ear.visualizer, 'Array', '192.168.0.150', 7777, 20, 7)
 Leds.add_device(Array)
+#Cloud = led.Cloud(ear.visualizer, 'Cloud', '192.168.0.150', 7777, [[140, 1]])
+#Leds.add_device(Cloud)
 
 
 fps = 120  #How often to update the FFT features + display
--- src/led_class.py
+++ src/led_class.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 
-class Led:
+class Led: 
     '''
     Main led driver class. Meant to be a parent class for led devices.
     Maintains a list of led devices used to synchronize and perform complex and cool displays
@@ -42,46 +42,90 @@ class Array(Device):
         self.bins = bins
         self.height = height
         self.cm = cm.hsv
-        self.colors = [list((255*np.array(self.cm(i))[:3]).astype(int)) for i in np.linspace(0,255,self.bins).astype(int)]
+        #PuBu
+        self.colors = [list((255*np.array(self.cm(i))[:3]).astype(int)) for i in np.linspace(0,255,self.bins*self.height).astype(int)]
+        self.colors = self.colors[::-1]
+        self.colors = self.colors[10:] + self.colors[:10]
 
     def generate_pixels(self):
         pixels = [round((item[3]/500)*(self.height))-1 for item in self.ear.fast_bars]
         pixels = pixels[::-1]
-        pixels = pixels[::1]
+        pixels = pixels[::2]
         #pixels = [max(pixels[i:i+3]) for i in range(0, len(pixels), 3)]
         rgb_leds = []
         for x in range(self.bins):
             for y in range(self.height):
+                n = x*self.height + y
                 if x%2==1:
                     y = self.height - 1 - y
                 if(y<pixels[x]):
-                    rgb_leds.append(self.colors[x][0])
-                    rgb_leds.append(self.colors[x][1])
-                    rgb_leds.append(self.colors[x][2])
+                    rgb_leds.append(self.colors[n][0])
+                    rgb_leds.append(self.colors[n][1])
+                    rgb_leds.append(self.colors[n][2])
                 else:
                     rgb_leds += [0,0,0]
+
+        #rgb_leds = rgb_leds[::-1]
+        #rgb_leds = 
 
 
         rgb_leds = bytes(rgb_leds)
         return rgb_leds
 
 
-shape= [[12, 3],
-        [14, 2],
-        [20, 5],
-        [34, 1],
-        [15, 4]]
 class Cloud(Device):
     def __init__(self, ear, device_type, ip, port, shape):
+        from matplotlib import cm
         super().__init__(ear, device_type, ip, port)
+        self.frequency_bins = len(ear.fast_bars)
         self.segments = shape
         self.total_leds = sum([x[0] for x in self.segments])
-        self.colors = [list((255*np.array(self.cm(i))[:3]).astype(int)) for i in np.linspace(0,255,self.total_leds).astype(int)]
-        self.bins_per_segment = ear.visualizer.n_frequency_bins // len(self.segments)
+        self.cm = cm.hsv
+        self.colors = [list((255*np.array(self.cm(i))[:3]).astype(int)) for i in np.linspace(0,255,500).astype(int)]
+        self.bins_per_segment = self.frequency_bins // len(self.segments)
+        self.mode = 'Demo2'
+        self.flag = 1
+        print(self.total_leds)
+        self.rgb_leds = []
+        for i in range(40):
+            self.rgb_leds.append(self.colors[i][0])
+            self.rgb_leds.append(self.colors[i][1])
+            self.rgb_leds.append(self.colors[i][2])
+        for i in range(self.total_leds-40):
+            self.rgb_leds += [0,0,0]
+        self.counter = 0
+        self.counter2 = 0
 
 
     def generate_pixels(self):
-        pixels = []
+        if self.mode == 'Demo':
+            if self.counter == 10:
+                self.rgb_leds = self.rgb_leds[3:] + self.rgb_leds[:3]
+                self.counter -= 10
+            self.counter += 1 
+        if self.mode == 'Demo2':
+            if self.counter == 20:
+                self.counter -= 20
+                self.rgb_leds = []
+                self.counter2 += 1
+                for i in range(self.total_leds):
+                    if self.counter2 == len(self.colors):
+                        self.counter2 -= len(self.colors)
+                    self.rgb_leds.append(self.colors[self.counter2][0])
+                    self.rgb_leds.append(self.colors[self.counter2][1])
+                    self.rgb_leds.append(self.colors[self.counter2][2])
+            self.counter += 1
+            #for i in range(300-self.total_leds):
+            #    rgb_leds += [0,0,0]
+        #else:        
+        #    for i in range(len(segments)):
+        #        frequency_bins = self.ear.fast_bars[self.bins_per_segment * (self.segments[i][1]-1) : (self.bins_per_segment+1) * (self.segments[i][1]-1)]
+        #        num_activated = max(frequency_bins)/500 * self.segments[i][0] - 1
 
         
-        return None
+        ret = bytes(self.rgb_leds)
+        return ret
+        
+
+        
+
